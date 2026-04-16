23a9331a2d0792d5f25fe52ea2a7c487dcf80243
ashleybcit <ashleydi.dev@gmail.com>
2026-04-16 15:13:16 -0700

    added bottoms images

--- src/components/GarmentPlaceholder.jsx
+++ src/components/GarmentPlaceholder.jsx
@@ -2,6 +2,18 @@ import styles from './ImagePlaceholder.module.css'
 import BottomsPear1 from '../assets/Bottoms-Pear-1.png'
 import BottomsPear2 from '../assets/Bottoms-Pear-2.png'
 import BottomsPear3 from '../assets/Bottoms-Pear-3.png'
+import BottomsApple1 from '../assets/Bottoms-Apple-1.png'
+import BottomsApple2 from '../assets/Bottoms-Apple-2.png'
+import BottomsHourglass1 from '../assets/Bottoms-Hourglass-1.png'
+import BottomsHourglass2 from '../assets/Bottoms-Hourglass-2.png'
+import BottomsHourglass3 from '../assets/Bottoms-Hourglass-3.png'
+import BottomsRectangle1 from '../assets/Bottoms-Rectangle-1.png'
+import BottomsRectangle2 from '../assets/Bottoms-Rectangle-2.png'
+import BottomsRectangle3 from '../assets/Bottoms-Rectangle-3.png'
+import BottomsInvertedTriangle1 from '../assets/Bottoms-InvertedTriangle-1.png'
+import BottomsInvertedTriangle2 from '../assets/Bottoms-InvertedTriangle-2.png'
+import BottomsInvertedTriangle3 from '../assets/Bottoms-InvertedTriangle-3.png'
+
 
 // Asset map: GarmentPlaceholder[category][bodyType] = [img1, img2, img3]
 // Add imports here as images are sourced.
@@ -12,7 +24,7 @@ import BottomsPear3 from '../assets/Bottoms-Pear-3.png'
 const GARMENT_ASSETS = {
   Tops:              { Hourglass: [null,null,null], Pear: [null,null,null], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
   Jackets:           { Hourglass: [null,null,null], Pear: [null,null,null], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
-  Bottoms:           { Hourglass: [null,null,null], Pear: [BottomsPear1,BottomsPear2,BottomsPear3], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
+  Bottoms:           { Hourglass: [BottomsHourglass1,BottomsHourglass2,BottomsHourglass3], Pear: [BottomsPear1,BottomsPear2,BottomsPear3], Apple: [BottomsApple1,BottomsApple2,null], Rectangle: [BottomsRectangle1,BottomsRectangle2,BottomsRectangle3], 'Inverted Triangle': [BottomsInvertedTriangle1,BottomsInvertedTriangle2,BottomsInvertedTriangle3] },
   Dresses:           { Hourglass: [null,null,null], Pear: [null,null,null], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
   Skirts:            { Hourglass: [null,null,null], Pear: [null,null,null], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
   Outerwear:         { Hourglass: [null,null,null], Pear: [null,null,null], Apple: [null,null,null], Rectangle: [null,null,null], 'Inverted Triangle': [null,null,null] },
